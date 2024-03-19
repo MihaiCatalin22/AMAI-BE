acfafad03c1192c2cb97de7742135683fdcd567d
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-03-19 16:25:14 +0100

    Implemented Agenda related classes

---
+++ AMAI-Project-2024/src/main/java/prjcb04/amaiproject2024/controller/AgendaController.java
@@ -0,0 +1,37 @@
+package prjcb04.amaiproject2024.controller;
+
+import prjcb04.amaiproject2024.domain.Agenda;
+import prjcb04.amaiproject2024.domain.Event;
+import prjcb04.amaiproject2024.business.AgendaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/agenda")
+public class AgendaController {
+
+    private final AgendaService agendaService;
+
+    @Autowired
+    public AgendaController(AgendaService agendaService) {
+        this.agendaService = agendaService;
+    }
+
+    @GetMapping("/upcoming")
+    public ResponseEntity<List<Event>> getUpcomingEvents() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Event> upcomingEvents = agendaService.getUpcomingEvents(now);
+        return ResponseEntity.ok(upcomingEvents);
+    }
+
+    @GetMapping("/past")
+    public ResponseEntity<List<Event>> getPastEvents() {
+        LocalDateTime now = LocalDateTime.now();
+        List<Event> pastEvents = agendaService.getPastEvents(now);
+        return ResponseEntity.ok(pastEvents);
+    }
+}
