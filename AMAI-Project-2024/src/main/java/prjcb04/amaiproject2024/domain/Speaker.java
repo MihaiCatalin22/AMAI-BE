7b842721f196cd0fcb6db91f39631ec6e9e84f89
Catalin Mihai Popoiu <117656515+MihaiCatalin20@users.noreply.github.com>
2024-03-19 17:48:51 +0100

    Implemented Presentation related classes

---
+++ AMAI-Project-2024/src/main/java/prjcb04/amaiproject2024/domain/Speaker.java
@@ -0,0 +1,29 @@
+package prjcb04.amaiproject2024.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import java.util.Set;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "speakers")
+@PrimaryKeyJoinColumn(name = "user_id")
+public class Speaker extends User {
+
+    private String bio;
+
+    private String expertiseArea;
+
+    @OneToMany(mappedBy = "speaker")
+    private Set<Presentation> presentations;
+}
